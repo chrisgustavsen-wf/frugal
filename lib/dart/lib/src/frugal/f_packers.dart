e60e27d244fd349502e93737f735ebd3e2ebffd1
Dusty Holmes <dusty.holmes@workiva.com>
2021-01-11 12:28:17 -0600

    Run the formatter

--- lib/dart/lib/src/frugal/f_packers.dart
+++ lib/dart/lib/src/frugal/f_packers.dart
@@ -4,7 +4,14 @@ import 'package:frugal/src/frugal.dart';
 import 'package:thrift/thrift.dart';
 
 /// Pack the arguments for transmission
-Uint8List prepareMessage(FContext ctx, String method, TBase args, int kind, FProtocolFactory pfactory, int limit) {
+Uint8List prepareMessage(
+  FContext ctx,
+  String method,
+  TBase args,
+  int kind,
+  FProtocolFactory pfactory,
+  int limit,
+) {
   final memoryBuffer = TMemoryOutputBuffer(limit);
   final oprot = pfactory.getProtocol(memoryBuffer);
   oprot.writeRequestHeader(ctx);
@@ -15,7 +22,12 @@ Uint8List prepareMessage(FContext ctx, String method, TBase args, int kind, FPro
 }
 
 /// Unpack the response into the [result]
-void processReply(FContext ctx, TBase result, TTransport response, FProtocolFactory pfactory) {
+void processReply(
+  FContext ctx,
+  TBase result,
+  TTransport response,
+  FProtocolFactory pfactory,
+) {
   final iprot = pfactory.getProtocol(response);
   iprot.readResponseHeader(ctx);
   final msg = iprot.readMessageBegin();
