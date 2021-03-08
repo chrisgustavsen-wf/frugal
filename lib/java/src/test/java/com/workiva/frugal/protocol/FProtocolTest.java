4d6943def71231ea0249c9b5d9baae3f98932c3c
Houston King <houston.king@workiva.com>
2021-03-08 15:23:46 -0700

    fix: add sanity test on FProtocol delegation

--- lib/java/src/test/java/com/workiva/frugal/protocol/FProtocolTest.java
+++ lib/java/src/test/java/com/workiva/frugal/protocol/FProtocolTest.java
@@ -41,6 +41,12 @@ public class FProtocolTest {
     }
 
     @Test
+    public void testGetMinSerializedSizeDelegates() throws Exception {
+        when(wrapped.getMinSerializedSize((byte)0)).thenReturn(42);
+        assertEquals(42, protocol.getMinSerializedSize((byte)0));
+    }
+
+    @Test
     public void testWriteRequestHeaders() throws Exception {
         when(wrapped.getTransport()).thenReturn(transport);
 
